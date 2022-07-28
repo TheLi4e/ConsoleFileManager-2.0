be15aeaeb9d1fb06f410aca7e19a06c616c966d1
Li4e99 <li4e99@gmail.com>
2022-07-28 17:34:31 +0200

    Добавьте файлы проекта.

---
+++ ConsoleFileManager 2.0/Commands/DirectoryDeleteCommand.cs
@@ -0,0 +1,62 @@
+﻿using FileManager.Commands.Base;
+
+
+namespace FileManager.Commands;
+
+public class DirectoryDeleteCommand : FileManagerCommand
+{
+    private readonly IUserInterface _UserInterface;
+    private readonly FileManagerLogic _FileManager;
+    public override string Description => "Удаление директории";
+    public DirectoryDeleteCommand(IUserInterface UserInterface, FileManagerLogic FileManager)
+    {
+        _UserInterface = UserInterface;
+        _FileManager = FileManager;
+    }
+    /// <summary>
+    /// Метод для подтверждения
+    /// </summary>
+    /// <param name="dir_path">Путь к файлу</param>
+    private void Aprove (string dir_path)
+    {
+        var aprove = _UserInterface.ReadLine($"Вы точно хотите удалить {dir_path} да/нет?", false).ToLower();
+        if (aprove == "да")
+        {
+            Directory.Delete(dir_path, true);
+            _UserInterface.WriteLine($"Файл {dir_path} удален.");
+        }
+        else
+            return;
+    }
+    public override void Execute(string[] args)
+    {
+
+        if (args.Length < 2 || string.IsNullOrWhiteSpace(args[1]))
+        {
+            _UserInterface.WriteLine("Для команды удаления директории необходимо указать один параметр - целевой каталог");
+            return;
+        }
+
+        var dir_path = args[1];
+        if (!Directory.Exists(dir_path))
+        {
+            
+            var args1 = String.Join(" ", args);
+            var dirWithSpace = args1.Split('\"');
+           
+            if (dirWithSpace.Length<2 || !Directory.Exists(dirWithSpace[1]))
+                _UserInterface.WriteLine("Указанная директория не существует.");
+
+            else
+                Aprove(dirWithSpace[1]);
+        }
+
+
+        else if (Directory.Exists(dir_path))
+        {
+            Aprove(dir_path);
+
+        }
+
+    }
+}
